3e0f4bb46e1af850169dc1258d20b83df2eb2b32
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-07-09 14:42:51 +0200

    Update KillerSpeed.lua

--- Things/Killer/KillerSpeed.lua
+++ Things/Killer/KillerSpeed.lua
@@ -5,39 +5,42 @@ local currentSession = os.clock()
 _G.ScriptSessionID = currentSession
 
 _G.KillerSpeed = _G.KillerSpeed or 5
+_G.SpeedToggle = _G.SpeedToggle or false
 local defaultSpeed = 16
 
-local function updateSpeed(character)
+local function monitorSpeed(character)
 	local humanoid = character:WaitForChild("Humanoid")
 	
-	if humanoid.WalkSpeed ~= _G.KillerSpeed then
-		defaultSpeed = humanoid.WalkSpeed
-	end
+	defaultSpeed = humanoid.WalkSpeed
 
-	task.spawn(function()
-		while _G.ScriptSessionID == currentSession and character:IsDescendantOf(workspace) and humanoid and humanoid.Health > 0 do
-			if LocalPlayer.Team and LocalPlayer.Team.Name == "Killer" then
+	if _G.SpeedConnection then _G.SpeedConnection:Disconnect() end
+	
+	_G.SpeedConnection = humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+		if _G.ScriptSessionID ~= currentSession then return end
+		
+		if _G.SpeedToggle and LocalPlayer.Team and LocalPlayer.Team.Name == "Killer" then
+			if humanoid.WalkSpeed ~= _G.KillerSpeed then
 				humanoid.WalkSpeed = _G.KillerSpeed
-			else
-				if humanoid.WalkSpeed ~= defaultSpeed then
-					humanoid.WalkSpeed = defaultSpeed
-				end
 			end
-			task.wait(0.1)
+		else
+			if humanoid.WalkSpeed == _G.KillerSpeed then
+				humanoid.WalkSpeed = defaultSpeed
+			end
 		end
 	end)
+
+	if _G.SpeedToggle and LocalPlayer.Team and LocalPlayer.Team.Name == "Killer" then
+		humanoid.WalkSpeed = _G.KillerSpeed
+	end
 end
 
 if LocalPlayer.Character then
-	updateSpeed(LocalPlayer.Character)
+	monitorSpeed(LocalPlayer.Character)
 end
 
-if _G.CharacterAddedConnection then
-	_G.CharacterAddedConnection:Disconnect()
-end
-
+if _G.CharacterAddedConnection then _G.CharacterAddedConnection:Disconnect() end
 _G.CharacterAddedConnection = LocalPlayer.CharacterAdded:Connect(function(character)
 	if _G.ScriptSessionID == currentSession then
-		updateSpeed(character)
+		monitorSpeed(character)
 	end
 end)
