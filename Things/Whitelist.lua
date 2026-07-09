f8b9ac968859801a9df6c0fbd8ceef74773340b0
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-07-09 18:59:40 +0200

    Update Whitelist.lua

--- Things/Whitelist.lua
+++ Things/Whitelist.lua
@@ -1,8 +1,7 @@
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
 
 if _G.PlayerListUpdaterRunning then
-    warn("PlayerListUpdater уже работает!")
+    warn("Updater уже работает!")
     return
 end
 _G.PlayerListUpdaterRunning = true
@@ -13,16 +12,15 @@ _G.SelectedPlayer = _G.SelectedPlayer or nil
 
 local function updatePlayerList()
     local newList = {}
-   
+    
     for _, plr in ipairs(Players:GetPlayers()) do
         if plr ~= Players.LocalPlayer then
             table.insert(newList, plr.Name)
         end
     end
-   
+    
     table.sort(newList)
     _G.PlayerList = newList
-   
     print("[PlayerUpdater] Обновлено: " .. #newList .. " игроков")
 end
 
