69959833a5a9f63b43b73742b8f917beb0b31283
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-07-10 22:16:25 +0200

    Update Violence District.lua

--- Violence District.lua
+++ Violence District.lua
@@ -28,6 +28,8 @@ local scripts = {
 	'Teleport/TpToGen.lua',
 	'Teleport/TpToHook.lua',
 	'Abilities/TheSlasher.lua',
+	'Abilities/TheStalker.lua',
+	'Abilities/TheKiller.lua',
 }
 
 local baseUrl = 'https://raw.githubusercontent.com/Fishka132312/Violence-District/refs/heads/main/Things/'
@@ -335,23 +337,10 @@ Tab:AddToggle({
     end
 })
 
-local Tab = Window:MakeTab({
-	Name = "Abilities",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
 local Section = Tab:AddSection({
-	Name = "The Slasher"
+	Name = "Abilities"
 })
 
-Tab:AddToggle({
-	Name = "Inf Abilities",
-	Default = false,
-	Callback = function(Value)
-		_G.Slasher1 = Value
-	end    
-})
 
 local Section = Tab:AddSection({
 	Name = "The Slasher"
@@ -376,6 +365,15 @@ Tab:AddToggle({
 		_G.Stalker = Value
 	end    
 })
+
+local Section = Tab:AddSection({
+	Name = "The Killer"
+})
+
+Tab:AddDropdown({Name = "Select Sound", Default = _G.SelectedSound or (_G.SoundOptions and _G.SoundOptions[1]) or "Sound 1", Options = _G.SoundOptions or {"Sound 1", "Sound 2", "Sound 3"}, Callback = function(Value) _G.SelectedSound = Value end})
+Tab:AddTextbox({Name = "Volume", Default = _G.Volume or "0.5", TextDisappear = true, Callback = function(Value) _G.Volume = Value end})
+Tab:AddTextbox({Name = "Distance", Default = _G.Distance or "50", TextDisappear = true, Callback = function(Value) _G.Distance = Value end})
+Tab:AddButton({Name = "Play Sound", Callback = function() if _G.PlaySound then _G.PlaySound() end end})
 -------------------------Survivors---------------------------
 
 local Tab = Window:MakeTab({
